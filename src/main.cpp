f7c67fec5cdd0981372f50a373ae258aafb30008
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-04 13:02:00 -0400

    add: menu with switch

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,37 @@ int main(){
     while(option!=5){
         cout<<"Bienvenido al reino!!!\nQue desea hacer hoy?"<<endl
         <<"1.-Convertir los datos en un arbol binario"<<endl
-        <<"2.-Mostrar la linea de ssucesion actual"<<endl
+        <<"2.-Mostrar la linea de sucesion actual"<<endl
         <<"3.-Asignar un nodo como rey de manera automática en caso de morir el rey actual"<<endl
         <<"4.-Cambiar los datos de cualquier nodo del árbol"<<endl
         <<"5.-Salir"<<endl;
         cin>>option;
 
-        if(option==5){
+        switch (option)
+        {
+        case 1:
+            cout<<"Se han convertido los datos en un arbol binario"<<endl;
+            break;
+        
+        case 2:
+            cout<<"La linea de sucesion actual es:\n"<<endl;
+            break;
+
+        case 3:
+            cout<<"rey asignado"<<endl;
+            break;
+
+        case 4:
+            cout<<"Se han cambiado los datos de manera exitosa"<<endl;
+            break;
+        
+        case 5:
             cout<<"Gracias por ejecutar el codigo del reino"<<endl;
+            break;
+            
+        default:
+            cout<<"Por favor ingrese un numero dentro del rango"<<endl;
+            break;
         }
     }
 }
