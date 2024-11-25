47be33a8c748527eae97885ac4d1b584e717ca8f
ValeskaJose <Valeskaafernandez@gmail.com>
2024-11-25 19:11:32 -0400

    fix: node class

--- src/node/node.h
+++ src/node/node.h
@@ -5,12 +5,25 @@ template<class T>
 class Node {
 private:
     T data;
+    int fe = 0;
     Node<T>* left = nullptr;
     Node<T>* right = nullptr;
+    int autoHeight ();
 public:
-    Node(T, Node<T>*, Node<T>*);
-    ~Node();
-    T getData();
-    void setData(T);
+    Node(T,Node<T>*,Node<T>*);
+        ~Node();
+        void setData(T);
+        T getData();
+        void setChildren(Node<T>*,Node<T>*);//Establece el hijo de un nodo
+        Node<T>* getChildren(int);//facilita el hijo de un nodo
+        void print();
+        void setFe(int);//permite establecer el factor de equilibrio
+        int getFe();//obtiene el factor de equilibrio
+        void setHeigth(int);//establece la altura del arbol
+        int calculate_Heigth(Node<T>*);//calcula la altura del arbol
+        int getHeigth();//obtiene la atura del arbol
+        bool isLeaf();//verifica si el nodo no tiene hijos
+        void setRight(Node<T>*);//establece el hijo derecho
+        void setLeft(Node<T>*);//establece el hijo izquierdo
 };
 #endif
