0c9b92676e088a59d1216efbe69a33ba5562f923
Carlos <CarlosOrChavez25@gmail.com>
2024-11-25 09:21:41 -0400

    Add: Search for children and firstborn

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,23 @@
 #include<string>
 #include<sstream>
 
- using namespace std;
+using namespace std;
 
 template<class T>
-class Node{
-    private:
+class Node {
+private:
     T data;
     Node<T>* left = nullptr;
     Node<T>* right = nullptr;
-    public:
-    Node(T,Node<T>*,Node<T>*);
+public:
+    Node(T, Node<T>*, Node<T>*);
     ~Node();
     T getData();
     void setData(T);
     void print();
 };
 
-class Persona{
+class Persona {
 public:
     int id;
     string first_name;
@@ -35,7 +35,7 @@ public:
     Persona* segundoHijo;
     Persona* Siguiente;
 
-    Persona(int _id, string _first_name, string _last_name, string _gender, int _age, int _id_father, bool _is_dead, bool _was_king, bool _is_king){
+    Persona(int _id, string _first_name, string _last_name, string _gender, int _age, int _id_father, bool _is_dead, bool _was_king, bool _is_king) {
         id = _id;
         first_name = _first_name;
         last_name = _last_name;
@@ -105,79 +105,89 @@ public:
 
 Persona* head = nullptr;
 void Menu(int option);
-void leerCSV (string KingDom);
+void leerCSV(string KingDom);
+void findChildrenById(string csvFile, int parentId);
 
-
-
-int main(){
+int main() {
     int option = 0;
-        Menu(option);
+    Menu(option);
+
+    int parentId;
+    cout << "Ingrese el ID del padre para buscar sus hijos: ";
+    cin >> parentId;
+    findChildrenById("../bin/KingDom.csv", parentId);
+
+    return 0;
 }
 
-void Menu(int option){
-    cout<<"Bienvenido al reino!!!\nQue desea hacer hoy?"<<endl;
-    while(option!=5){
-        cout<<"1.-Convertir los datos en un arbol binario"<<endl
-        <<"2.-Mostrar la linea de sucesion actual"<<endl
-        <<"3.-Asignar un nodo como rey de manera automatica en caso de morir el rey actual"<<endl
-        <<"4.-Cambiar los datos de cualquier nodo del arbol"<<endl
-        <<"5.-Salir"<<endl;
-        cin>>option;
-    switch (option)
-        {
-        case 1:
-        leerCSV("KingDom.csv");
-            cout<<"Se han convertido los datos en un arbol binario"<<endl;
-            break;
-        
-        case 2:
-            cout<<"La linea de sucesion actual es:\n"<<endl;
-            break;
+void Menu(int option) {
+    cout << "Bienvenido al reino!!!\nQue desea hacer hoy?" << endl;
+    while (option != 5) {
+        cout << "1.-Convertir los datos en un arbol binario" << endl
+             << "2.-Mostrar la linea de sucesion actual" << endl
+             << "3.-Asignar un nodo como rey de manera automatica en caso de morir el rey actual" << endl
+             << "4.-Cambiar los datos de cualquier nodo del arbol" << endl
+             << "5.-Salir" << endl;
+        cin >> option;
+        switch (option) {
+            case 1:
+                leerCSV("../bin/KingDom.csv");
+                cout << "Se han convertido los datos en un arbol binario" << endl;
+                break;
 
-        case 3:
-            cout<<"rey asignado"<<endl;
-            break;
+            case 2:
+                int parentId;
+                cout << "Ingrese el ID del padre: ";
+                cin >> parentId;
+                findChildrenById("../bin/KingDom.csv", parentId);
+                cout << "La linea de sucesion actual es:\n" << endl;
+                break;
 
-        case 4:
-            cout<<"Se han cambiado los datos de manera exitosa"<<endl;
-            break;
-        
-        case 5:
-            cout<<"Gracias por ejecutar el codigo del reino"<<endl;
-            break;
+            case 3:
+                cout << "rey asignado" << endl;
+                break;
 
-        default:
-            cout<<"Por favor ingrese un numero dentro del rango\n"<<endl;
-            break;
+            case 4:
+                cout << "Se han cambiado los datos de manera exitosa" << endl;
+                break;
+
+            case 5:
+                cout << "Gracias por ejecutar el codigo del reino" << endl;
+                break;
+
+            default:
+                cout << "Por favor ingrese un numero dentro del rango\n" << endl;
+                break;
         }
-        }
+    }
 }
 
 template<class T>
-Node<T>::Node(T data,Node<T>* left,Node<T>* rigth){
-    if(left != NULL){
+Node<T>::Node(T data, Node<T>* left, Node<T>* right) {
+    if (left != NULL) {
         this->left = left;
     }
-    if(right != NULL){
-        this->right = rigth;
+    if (right != NULL) {
+        this->right = right;
     }
 }
 
 template<class T>
-Node<T>::~Node(){
+Node<T>::~Node() {
     this->left = nullptr;
     this->right = nullptr;
 }
 
 template<class T>
-T Node<T>::getData(){
+T Node<T>::getData() {
     return this->data;
 }
 
 template<class T>
-void Node<T>::setData(T data){
+void Node<T>::setData(T data) {
     this->data = data;
 }
+
 void leerCSV(string KingDom) {
     FamilyTree tree;
 
@@ -218,5 +228,70 @@ void leerCSV(string KingDom) {
         cout << "La persona con ID " << id << " es el primogénito de " << father->first_name << " " << father->last_name << "." << endl;
     } else {
         cout << "La persona con ID " << id << " no es el primogénito de nadie o no tiene padre registrado." << endl;
+    }
+}
+
+void findChildrenById(string csvFile, int parentId) {
+    ifstream file(csvFile);
+    if (!file.is_open()) {
+        cout << "Error al abrir el archivo " << csvFile << endl;
+        return;
+    }
+
+    FamilyTree tree;
+    string line;
+    getline(file, line); // Saltar la primera línea (encabezados)
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string field[9];
+        for (int i = 0; i < 9; ++i) {
+            getline(ss, field[i], ',');
+        }
+
+        Persona* nuevaPersona = new Persona(
+            stoi(field[0]),
+            field[1],
+            field[2],
+            field[3],
+            stoi(field[4]),
+            stoi(field[5]),
+            stoi(field[6]),
+            stoi(field[7]),
+            stoi(field[8])
+        );
+
+        tree.addPerson(nuevaPersona);
+    }
+
+    file.close();
+
+    Persona* parent = tree.findPersonById(parentId);
+    if (parent == nullptr) {
+        cout << "No se encontró a la persona con ID " << parentId << endl;
+        return;
+    }
+
+    cout << "Buscando hijos de: " << parent->first_name << " " << parent->last_name << endl;
+
+    Persona* oldestChild = nullptr;
+    bool hasChildren = false;
+
+    Persona* temp = tree.head;
+    while (temp != nullptr) {
+        if (temp->id_father == parentId) {
+            hasChildren = true;
+            cout << "Hijo: " << temp->first_name << " " << temp->last_name << endl;
+            if (oldestChild == nullptr || temp->age > oldestChild->age) {
+                oldestChild = temp;
+            }
+        }
+        temp = temp->Siguiente;
+    }
+
+    if (!hasChildren) {
+        cout << "La persona con ID " << parentId << " no tiene hijos registrados." << endl;
+    } else if (oldestChild != nullptr) {
+        cout << "El hijo de mayor edad es: " << oldestChild->first_name << " " << oldestChild->last_name << " con " << oldestChild->age << " años." << endl;
     }
 }
